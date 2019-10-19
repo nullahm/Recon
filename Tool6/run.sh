5049ab9030c71e96b1dccf8c5d467d7edf7d4221
TiLAK Patel <patel_tlk@yahoo.com>
2019-10-19 18:16:49 +0530

    Integrated Keyword Search tool

--- Tool6/run.sh
+++ Tool6/run.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
 echo "Executing Tool \"Search for Interesting Keywords & Comments\""
+
+printf ""
+printf "Provide Full Path to file containing URLs List: "
+read file
+
+python "$PWD"/Tool6/keyword_search.py $file
