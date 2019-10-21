161ed6f4e5805b47455ea8b2f0cb49a617e68a63
kushgo13 <56816393+kushgo13@users.noreply.github.com>
2019-10-21 14:31:13 +0530

    Update run.sh

--- Tool5/run.sh
+++ Tool5/run.sh
@@ -1,3 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 
-echo "Executing Tool \"Search Wayback urls\""
+curl -s "http://web.archive.org/cdx/search/cdx?url=$1" | grep -oE http.*://.*/ | sort -u > $1.txt
+
+echo "check out $1.txt"
